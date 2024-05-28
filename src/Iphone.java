99194548d05a91923e24a7868b1a584445691e9b
piedrohammer <piiedrohammer@gmail.com>
2024-05-28 14:13:33 -0300

    Desafio Dio

---
+++ src/Iphone.java
@@ -0,0 +1,49 @@
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+        // Implementação dos métodos de ReprodutorMusical
+        @Override
+        public void tocar() {
+            System.out.println("Tocando música...");
+        }
+
+        @Override
+        public void pausar() {
+            System.out.println("Música pausada.");
+        }
+
+        @Override
+        public void selecionarMusica(String musica) {
+            System.out.println("Música selecionada: " + musica);
+        }
+
+        // Implementação dos métodos de AparelhoTelefonico
+        @Override
+        public void ligar(String numero) {
+            System.out.println("Ligando para: " + numero);
+        }
+
+        @Override
+        public void atender() {
+            System.out.println("Atendendo chamada...");
+        }
+
+        @Override
+        public void iniciarCorreioVoz() {
+            System.out.println("Iniciando correio de voz...");
+        }
+
+        // Implementação dos métodos de NavegadorInternet
+        @Override
+        public void exibirPagina(String url) {
+            System.out.println("Exibindo página: " + url);
+        }
+
+        @Override
+        public void adicionarNovaAba() {
+            System.out.println("Nova aba adicionada.");
+        }
+
+        @Override
+        public void atualizarPagina() {
+            System.out.println("Página atualizada.");
+        }
+    }
